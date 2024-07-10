76ada2b1c0d3435b77691e60eef853e087acc324
ajaykumarchawla <ajaychawla804@gmail.com>
2024-07-11 00:31:36 +0200

    updated working traefik

--- install_certificates.sh
+++ install_certificates.sh
@@ -2,4 +2,4 @@ mkcert -install
 if [ ! -d "certs" ]; then
     mkdir certs
 fi
-mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "maestro.localhost" "172.30.0.1"
+mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "maestro.localhost" "127.0.1.1"
--- setup_components.sh
+++ setup_components.sh
@@ -9,4 +9,5 @@ fi
 docker compose -f docker-compose.yml up --build -d
 docker compose -f ../maestro-issues-db/docker-compose.yml up --build -d
 docker compose -f ../maestro-ArchUI/docker-compose.yml up --build -d
+# docker compose -f ../maestro-dl-manager/docker-compose-no-gpu.yml up --build -d
 docker compose -f ../maestro-search-engine/docker-compose.yml up --build -d
